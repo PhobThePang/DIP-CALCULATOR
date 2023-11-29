62fe3d852400342886c8fcd19ab96287ac1dcf90
PhobthePang <naphaksopro@gmail.com>
2023-11-29 10:50:16 +0700

    change detail

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ class MyCalculator:
     def __init__(self):
         self.root = tk.Tk()
         self.root.geometry("1080x480")
-        self.root.title("Calculator Pro X +++")
+        self.root.title("DIP CAL")
 
-        self.label = tk.Label(self.root, text="Calculator Pro X +++", font=('Comic Sans MS', 69))
+        self.label = tk.Label(self.root, text="Hellow DIP 01", font=('Comic Sans MS', 69))
         self.label.pack()
-        self.button = tk.Button(self.root, text="Download Here", font=('Comic Sans MS', 10))
+        self.button = tk.Button(self.root, text="Click here", font=('Comic Sans MS', 10))
         self.button.place(x=500, y=150)
 
 
